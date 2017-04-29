c21d4e7842c78a9abf576ff12e606bf4089a5f44
mwdeal1 <mwdeal1@gmail.com>
2017-04-29 13:13:16 -0400

    First Working Version

---
+++ app/components/Main.js
@@ -0,0 +1,28 @@
+var React = require("react");
+var helpers = require("./utils/helpers");
+
+var Main = React.createClass({
+    // Here we render the function
+    render: function () {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="jumbotron">
+                        <h2 className="text-center">This is a test</h2>
+                        <p className="text-center">
+                            <em>There should be some google stuff below here</em>
+                        </p>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="g-signin2" data-onsuccess="onSignIn"></div>
+                    <a href="#" onClick="signOut();">Sign out</a>
+                </div>
+            </div>
+        );
+    }
+});
+
+// Export the component back for use in other files
+module.exports = Main;
